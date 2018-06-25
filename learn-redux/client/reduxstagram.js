6293847f47aaede07aba3712c79ea454fa746b43
Cameron Pickle <cmpickle@gmail.com>
2018-06-25 10:12:06 -0600

    starting reduxtagram

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+render(<p>hello world</p>, document.getElementById('root'));
