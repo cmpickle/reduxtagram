7f6d5d60145daff98ceddefe184b5dd673b2634f
Cameron Pickle <cmpickle@gmail.com>
2018-06-25 11:32:00 -0600

    create react store

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -5,3 +5,17 @@ import { browserHistory } from 'react-router';
 // Import the root reducer
 import rootReducer from './reducers/index';
 
+import comments from './data/comments';
+import posts from './data/posts';
+
+// Create an object for the default data
+const defaultState = {
+    posts,
+    comments,
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
