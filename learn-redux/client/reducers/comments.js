0ea67caa84b01cc1608f778183674f5a55d110e1
Cameron Pickle <cmpickle@gmail.com>
2018-06-26 10:09:59 -0600

    redux reducer composition

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,32 @@
+function postcomments(state = [], action) {
+    switch(action.type) {
+        case 'ADD_COMMENT':
+            // return the new state with the new comment
+            return [...state, {
+                user: action.author,
+                text: action.comment,
+            }];
+        case 'REMOVE_COMMENT':
+            return [
+                // from the start to the one we want to delete
+                ...state.slice(0, action.i),
+                // after the deleted one to the end
+                ...state.slice(action.i +1),
+            ]
+        default:
+            return state;
+    }
+}
+
 function comments(state = [], action) {
-    console.log(state, action);
+    if(typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postcomments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
